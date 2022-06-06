090e7949711085e2b2db835322d69b65f7a1f443
Bernard Leanse <bernardleanse96@gmail.com>
2022-06-06 13:40:27 +0100

    heavy refactor

--- printer/printer.py
+++ printer/printer.py
@@ -1,3 +1,27 @@
+INT_TO_WORDS = {
+      1:"one",
+      2:"two",
+      3:"three",
+      4:"four",
+      5:"five",
+      6:"six",
+      7:"seven",
+      8:"eight",
+      9:"nine",
+      0:"zero"
+    }
+
 class Printer:
+  def create_ascending_array(number):
+    numbers = []
+    for num in range(1, number + 1):
+      numbers.append(str(num) + '\n')
+    return numbers
+
+  def convert_int_to_word(int):
+    return INT_TO_WORDS[int]
+
   def generate_string_to_print(self, number):
-    return '1\n2\nthree'
+    numbers = Printer.create_ascending_array(number)
+    numbers[-1] = Printer.convert_int_to_word(number)
+    return ''.join(numbers)
