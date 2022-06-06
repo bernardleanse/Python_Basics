92aa5321cc8f5f370739239dcd8eda0084847e5b
Bernard Leanse <bernardleanse96@gmail.com>
2022-06-06 13:21:25 +0100

    test passes

---
+++ printer/printer.py
@@ -0,0 +1,3 @@
+class Printer:
+  def generate_string_to_print(self, number):
+    return '1\n2\nthree'
---
+++ printer/test_printer.py
@@ -0,0 +1,7 @@
+from printer import Printer
+
+class TestPrinter:
+  def test_it_returns_simple_format(self):
+    ptr = Printer()
+    assert ptr.generate_string_to_print(3) == '1\n2\nthree'
+
