1b508bcc1a3562fb23c29bee5438be6d0b8460c8
Bernard Leanse <bernardleanse96@gmail.com>
2022-06-06 12:09:19 +0100

    test basic slice

--- test_slicer.py
+++ test_slicer.py
@@ -1,6 +1,6 @@
 from slicer import Slicer
 
 class TestSlicer:
-	def test_slice(self):
+	def test_slice_takes_first_two_chars_of_string(self):
 		slicer = Slicer()
-		assert True
+		assert slicer.slice(0,2,"hello") == "he"
