e92cee610cdbb92ee6b98f64493093a3c6b805d0
Bernard Leanse <bernardleanse96@gmail.com>
2022-06-06 12:14:36 +0100

    more complex test

--- test_slicer.py
+++ test_slicer.py
@@ -4,3 +4,6 @@ class TestSlicer:
 	def test_slice_takes_first_two_chars_of_string(self):
 		slicer = Slicer()
 		assert slicer.slice(0,2,"hello") == "he"
+	def test_more_complex_example(self):
+		slicer = Slicer()
+		assert slicer.slice(2, 5, "bigboystring") == "gbo"
